09e38d0b359fed9ef0be152019ed5f7b84e3527f
IBodmer <pewpew308@gmail.com>
2023-03-12 18:39:41 +0300

    yaml макаки на острие атаки2

--- src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
@@ -1,10 +1,17 @@
 package jm.task.core.jdbc.dao;
 
 import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.util.Util;
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 
 import java.util.List;
 
 public class UserDaoHibernateImpl implements UserDao {
+    private final SessionFactory sf = Util.getConnection();
+
     public UserDaoHibernateImpl() {
 
     }
@@ -12,31 +19,95 @@ public class UserDaoHibernateImpl implements UserDao {
 
     @Override
     public void createUsersTable() {
-
+        Session currentSession = sf.getCurrentSession();
+        Transaction transaction = null;
+        try (currentSession) {
+            transaction = currentSession.beginTransaction();
+            currentSession.createNativeQuery("CREATE TABLE IF NOT EXISTS users " +
+                    "(id BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY, " +
+                    "user_name VARCHAR(50),last_name VARCHAR(50),age INT )").executeUpdate();
+            transaction.commit();
+            System.out.println("таблица создалась");
+        } catch (HibernateException e) {
+            System.out.println(e.getMessage());
+            if (transaction != null) transaction.rollback();
+        }
     }
 
     @Override
     public void dropUsersTable() {
-
+        Session currentSession = sf.getCurrentSession();
+        Transaction transaction = null;
+        try (currentSession) {
+            transaction = currentSession.beginTransaction();
+            currentSession.createNativeQuery("DROP TABLE IF EXISTS users").executeUpdate();
+            transaction.commit();
+            System.out.println("таблицу дропнули");
+        } catch (HibernateException e) {
+            System.out.println(e.getMessage());
+            if (transaction != null) transaction.rollback();
+        }
     }
 
     @Override
     public void saveUser(String name, String lastName, byte age) {
-
+        Session currentSession = sf.getCurrentSession();
+        Transaction transaction = null;
+        try (currentSession) {
+            transaction = currentSession.beginTransaction();
+            currentSession.save(new User(name, lastName, age));
+            transaction.commit();
+            System.out.println("User с именем – " + name + " добавлен в базу данных");
+        } catch (HibernateException e) {
+            System.out.println(e.getMessage());
+            if (transaction != null) transaction.rollback();
+        }
     }
 
     @Override
     public void removeUserById(long id) {
-
+        Session currentSession = sf.getCurrentSession();
+        Transaction transaction = null;
+        try (currentSession) {
+            transaction = currentSession.beginTransaction();
+            currentSession.delete(currentSession.get(User.class, id));
+            transaction.commit();
+            System.out.println("Юзер удален");
+        } catch (HibernateException e) {
+            System.out.println(e.getMessage());
+            if (transaction != null) transaction.rollback();
+        }
     }
 
     @Override
     public List<User> getAllUsers() {
-        return null;
+        Session currentSession = sf.getCurrentSession();
+        Transaction transaction = null;
+        List<User> list = null;
+        try (currentSession) {
+            transaction = currentSession.beginTransaction();
+            list = currentSession.createNativeQuery("SELECT * FROM users", User.class).getResultList();
+            transaction.commit();
+            return list;
+        } catch (HibernateException e) {
+            System.out.println(e.getMessage());
+            if (transaction != null) transaction.rollback();
+        }
+        return list;
     }
 
     @Override
     public void cleanUsersTable() {
-
+        Session currentSession = sf.getCurrentSession();
+        Transaction transaction = null;
+        try (currentSession) {
+            transaction = currentSession.beginTransaction();
+            currentSession.createNativeQuery("TRUNCATE users").executeUpdate();
+            transaction.commit();
+            System.out.println("таблицу обнулили");
+        } catch (HibernateException e) {
+            System.out.println(e.getMessage());
+            if (transaction != null) transaction.rollback();
+        }
     }
 }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,12 +1,19 @@
 package jm.task.core.jdbc.util;
 
+import jm.task.core.jdbc.model.User;
+import org.hibernate.HibernateException;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.service.ServiceRegistry;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
 
 public class Util {
-    public static Connection getConnection() {
+    public static Connection getConnectionJDBC() {
         Connection conn;
         try {
             conn = DriverManager.getConnection("jdbc:postgresql://localhost:5432/homeWorkDB", "postgres", "11235813");
@@ -16,4 +23,22 @@ public class Util {
         return conn;
     }
 
+    public static SessionFactory getConnection() {
+
+        SessionFactory sessionFactory = null;
+        try {
+            Configuration configuration = new Configuration()
+                    .setProperty("hibernate.connection.driver_class", "org.postgresql.Driver")
+                    .setProperty("hibernate.connection.url", "jdbc:postgresql://localhost:5432/homeWorkDB")
+                    .setProperty("hibernate.connection.username", "postgres")
+                    .setProperty("hibernate.connection.password", "11235813")
+                    .setProperty("hibernate.dialect", "org.hibernate.dialect.PostgreSQL82Dialect")
+                    .setProperty("hibernate.current_session_context_class","thread")
+                    .addAnnotatedClass(User.class);
+            sessionFactory = configuration.buildSessionFactory();
+        } catch (HibernateException e) {
+            e.printStackTrace();
+        }
+        return sessionFactory;
+    }
 }
